5c019a5000d9832f6dc0ba04f1716461024664c6
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-03-29 15:08:04 -0700

    updated returns existing user test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,23 @@ const request = require('supertest');
 const app = require('../lib/app');
 const UserService = require('../lib/services/UserService');
 
+const testUser = {
+  email: 'ernie@longdog.com',
+  password: 'littlekitty1'
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? testUser.password;
+
+  const agent = request.agent(app);
+
+  const user = await UserService.create({ ...testUser, ...userProps });
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
+
 describe('backend-top-secrets routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -16,30 +33,21 @@ describe('backend-top-secrets routes', () => {
   it('registers a user with an email and password', async () => {
     const res = await request(app)
       .post('/api/v1/users')
-      .send({ 
-        email: 'ernie@longdog.com',
-        password: 'littlekitty1'
-      });
+      .send(testUser);
 
-    expect(res.body).toEqual({ id: expect.any(String), email: 'ernie@longdog.com' });
+    const { email } = testUser;
+
+    expect(res.body).toEqual({ id: expect.any(String), email });
   });
 
-  it('logs in an existing user with email and password', async () => {
-    const user = await UserService.create({
-      email: 'ernie@longdog.com',
-      password: 'littlekitty1'
-    });
+  it('logs in an existing user with email and password and returns current user', async () => {
+    const [agent, user] = await registerAndLogin();
+    const thisUser = await agent.get('/api/v1/users/id');
 
-    const res = await request(app)
-      .post('/api/v1/users/sessions')
-      .send({
-        email: 'ernie@longdog.com',
-        password: 'littlekitty1'
-      });
-
-    expect(res.body).toEqual({
-      message: 'Signed in, welcome back!',
-      user
+    expect(thisUser.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number)
     });
   });
 
