859b181f796215831769821dfa86aa7d31348e72
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-03-29 12:50:11 -0700

    wrote test for logging in an existing user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('backend-top-secrets routes', () => {
   beforeEach(() => {
@@ -21,6 +22,25 @@ describe('backend-top-secrets routes', () => {
       });
 
     expect(res.body).toEqual({ id: expect.any(String), email: 'ernie@longdog.com' });
+  });
+
+  it('logs in an existing user with email and password', async () => {
+    const user = await UserService.create({
+      email: 'ernie@longdog.com',
+      password: 'littlekitty1'
+    });
+
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({
+        email: 'ernie@longdog.com',
+        password: 'littlekitty1'
+      });
+
+    expect(res.body).toEqual({
+      message: 'Signed in, welcome back!',
+      user
+    });
   });
 
 });
