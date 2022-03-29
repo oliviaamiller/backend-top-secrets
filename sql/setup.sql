2cee2fe81ec082d54bcc3ce39eabd15e83bea6f1
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-03-29 12:09:48 -0700

    wrote test for registering new users with email and password. created users sql table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,15 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
+
+DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS secrets CASCADE;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL
+);
 
 CREATE TABLE secrets (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
